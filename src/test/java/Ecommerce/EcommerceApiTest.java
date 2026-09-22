c6cf216c3887c63d8c6ef9a3687de170715906e4
Bharat Poojary <bharatpoojary111@gmail.com>
2026-09-22 18:05:20 +0530

    Ecommerce

---
+++ src/test/java/Ecommerce/EcommerceApiTest.java
@@ -0,0 +1,10 @@
+package Ecommerce;
+
+public class EcommerceApiTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/Pojo/LoginRequest.java
@@ -0,0 +1,19 @@
+package Pojo;
+
+public class LoginRequest {
+	
+	String userName;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	String password;
+}
